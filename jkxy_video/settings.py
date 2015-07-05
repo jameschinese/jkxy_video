7b965a3c4cbf69d98fbed48021ba0eb0e6600d6e
董章泉 <Quan@Quan-Mac.local>
2015-07-05 23:36:41 +0800

    创建项目

---
+++ jkxy_video/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for jkxy_video project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'jkxy_video'
+
+SPIDER_MODULES = ['jkxy_video.spiders']
+NEWSPIDER_MODULE = 'jkxy_video.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'jkxy_video (+http://www.yourdomain.com)'
